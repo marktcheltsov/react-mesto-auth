167dd91a6bd2a16e3e3f099aaa88e18b96fb5276
marktcheltsov <104628734+marktcheltsov@users.noreply.github.com>
2022-11-15 15:18:59 +0300

    Delete src/utils directory

--- src/utils/utils.js
+++
@@ -1,7 +0,0 @@
-export function renderLoading(isLoading, btn, text) {
-    if (isLoading === true) {
-      btn.textContent = 'Сохранение...';
-    } else if (isLoading === false) {
-      btn.textContent = text;
-    }
-  }
